5e65586ea9cbb92527caf3bee9dfced351b9f27b
bartoszryfa <bartecki77@icloud.com>
2023-11-30 21:54:43 +0100

    Add hibernate

--- src/main/java/pl/bartoszryfa/prakSystemApp/Parking.java
+++ src/main/java/pl/bartoszryfa/prakSystemApp/Parking.java
@@ -1,17 +1,27 @@
 package pl.bartoszryfa.prakSystemApp;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="parking")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Parking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
+    @Column(name="registrationNumber")
     private String registrationNumber;
+    @Column(name="time")
     private LocalDateTime startTime;
 }
 
---
+++ src/main/java/pl/bartoszryfa/prakSystemApp/database/DatabaseAdder.java
@@ -0,0 +1,4 @@
+package pl.bartoszryfa.prakSystemApp.database;
+
+public class DatabaseAdder {
+}
