4ec767c85bdfc7598ceab8e5b35a3a7b2d14014a
bartoszryfa <bartecki77@icloud.com>
2023-11-30 18:59:03 +0100

    Add first file

---
+++ src/main/java/pl/bartoszryfa/prakSystemApp/PrakSystemAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bartoszryfa.prakSystemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrakSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrakSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bartoszryfa/prakSystemApp/PrakSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartoszryfa.prakSystemApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrakSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
